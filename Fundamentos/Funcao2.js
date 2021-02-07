128c0223f7879b7888b1bff57b2b6e513aeaeb11
Santiago Augusto de Souza Monteiro <santiago.a.monteiro@gmail.com>
2021-02-06 22:12:33 -0300

    Funcao2

---
+++ Fundamentos/Funcao2.js
@@ -0,0 +1,22 @@
+// Armazenado uma função em uma variável
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// armazenando uma função arrow em uma variável
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// retorno implícito 
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(2, 3))
+
+const imprime2 = a => console.log(a)
+imprime2('Legal!!!')
+
